4c7fcf331b43dfd953e411cbfe521c4b0892c12a
Sebs <sebastianruizj2014@gmail.com>
2023-07-27 16:52:56 -0500

    Repositorio
    Repositorio configurado

---
+++ src/main/java/net/rj/repository/CategoriasRepository.java
@@ -0,0 +1,9 @@
+package net.rj.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import net.rj.model.Categoria;
+
+public interface CategoriasRepository extends CrudRepository<Categoria, Integer> {
+
+}
