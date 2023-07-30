dac87767464cab312a55954687dc57b4e2406424
Sebs <sebastianruizj2014@gmail.com>
2023-07-29 20:27:31 -0500

    CRUD

--- src/main/java/net/rj/JpaDemoApplication.java
+++ src/main/java/net/rj/JpaDemoApplication.java
@@ -1,10 +1,13 @@
 package net.rj;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import net.rj.model.Categoria;
 import net.rj.repository.CategoriasRepository;
 
 @SpringBootApplication
@@ -18,11 +21,53 @@ public class JpaDemoApplication implements CommandLineRunner{
 	}
 
 	public void run(String... args) throws Exception {
-		System.out.println(repo);
+		modificar(2);
 	}
 
+	// Create
 	private void guardar() {
-		System.out.println("Insertando un registro...");
+		Categoria cat = new Categoria();
+		
+		// No asignamos un id porque es nuestra llave primaria AUTOINCREMENTABLE
+		cat.setNombre("Finanzas");
+		cat.setDescripcion("Trabajos relacionados con finanzas y contabilidad");
+		
+		repo.save(cat);
+		
+		System.out.println(cat);
+	}
+	
+	// Read
+	private void buscarPorId(Integer id) {
+		Optional<Categoria> optional = repo.findById(id);
+		
+		// Validamos si el objeto existe
+		if (optional.isPresent()) {
+			System.out.println(optional);
+		} else {
+			System.out.println("Categoria no encontrada");
+		}
+		
+	}
+	
+	// Modificar
+	private void modificar(Integer id) {
+		Optional<Categoria> optional = repo.findById(id);
+				
+		// Validamos si el objeto existe
+		if (optional.isPresent()) {
+			Categoria catTmp = optional.get(); // Se recupera el objeto
+			// Realizamos los cambios
+			catTmp.setNombre("Ing. Software");
+			catTmp.setDescripcion("Desarrollo de sistemas");
+			
+			// Posteriormente se guarda para ser actualizado
+			repo.save(catTmp);
+			
+			System.out.println(optional);
+		} else {
+			System.out.println("Categoria no encontrada");
+		}
 	}
 	
 }
